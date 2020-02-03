24e3fac11a23ff9b16138e05df964e02c7fe7552
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-03 18:58:37 +0300

    adding basic UI

---
+++ lib/pressentation/about.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:nasa/pressentation/common/basicappbar.dart';
+import 'package:nasa/pressentation/common/buttomNavigation.dart';
+import 'package:nasa/pressentation/common/sidebar.dart';
+
+class AboutPage extends StatefulWidget {
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(235, 237, 240, 1.0),
+      appBar: BasicAppBar.getAppBar(context),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/birthday.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(1),
+                          Colors.black12,
+                          Colors.black45
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      "About",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/lab.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(1),
+                          Colors.black12,
+                          Colors.black45
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      "Objective",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/labcolor.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(1),
+                          Colors.black12,
+                          Colors.black45
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      "Misssion",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/water.JPG'),
+                        fit: BoxFit.fill,
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(1),
+                          Colors.black12,
+                          Colors.black45
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      "Vission",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      drawer: SideBarDrawer(),
+      bottomNavigationBar:
+          ButtomNavigationBarContainer.getButtomNavigationBar(context),
+    );
+  }
+}
