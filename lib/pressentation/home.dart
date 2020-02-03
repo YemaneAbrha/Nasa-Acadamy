911a2da69df5510a87e13fb981f7200e7cdd4669
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-03 18:51:24 +0300

    complete the home page

--- lib/pressentation/home.dart
+++ lib/pressentation/home.dart
@@ -12,10 +12,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(235, 237, 240, 1.0),
       appBar: BasicAppBar.getAppBar(context),
       body: Container(
         child: ListView(
           children: <Widget>[
+            SizedBox(
+              height: 10.0,
+            ),
             SizedBox(
               width: 200,
               height: 200,
@@ -27,26 +31,157 @@ class _HomePageState extends State<HomePage> {
                     color: Colors.white,
                   ),
                   Container(
-                    child: Image(),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(5.0),
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
                     alignment: Alignment.bottomCenter,
                     decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/birthday.jpg'),
+                        fit: BoxFit.fill,
+                      ),
                       gradient: LinearGradient(
                         begin: Alignment.topCenter,
                         end: Alignment.bottomCenter,
                         colors: <Color>[
-                          Colors.black.withAlpha(0),
+                          Colors.black.withAlpha(1),
                           Colors.black12,
                           Colors.black45
                         ],
                       ),
                     ),
                     child: Text(
-                      "This will be the description part",
+                      "የ ናሳ ትምህርት ቤት ተማሪዎች ልደትን ሲያከብሩ",
                       style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/lab.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(1),
+                          Colors.black12,
+                          Colors.black45
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      "የ ናሳ ትምህርት ቤት ተማሪዎች ቤተ፡ሙከራ ላይ ሁነው",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/labcolor.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(1),
+                          Colors.black12,
+                          Colors.black45
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      "የ ናሳ ትምህርት ቤት ተማሪዎች ቤተ፡ሙከራ ላይ ሁነው",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage('assets/images/water.JPG'),
+                        fit: BoxFit.fill,
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(1),
+                          Colors.black12,
+                          Colors.black45
+                        ],
+                      ),
+                    ),
+                    child: Text(
+                      " የ  ናሳ ትምህርት ቤት ተማሪዎች water park ሂደው ሲዝናኑ",
+                      style: TextStyle(
+                        color: Colors.black,
                         fontSize: 20.0,
                       ),
                     ),
