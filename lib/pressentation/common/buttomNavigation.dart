a67d41f382e8655ca4f388a2b7221add92b18e22
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-03 11:42:17 +0300

    adding bottom navigation

--- lib/pressentation/common/buttomNavigation.dart
+++ lib/pressentation/common/buttomNavigation.dart
@@ -1,2 +1,24 @@
 // buttom Navigation bar
 // contain the buttom navigation
+import 'package:flutter/material.dart';
+
+class ButtomNavigationBarContainer {
+  static getButtomNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.message),
+          title: Text('Message'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text("Home"),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          title: Text("Notification"),
+        ),
+      ],
+    );
+  }
+}
