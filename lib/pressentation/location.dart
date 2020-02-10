90abfc2dbd5c8160dd8a5d79af21f78b5ba1d338
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-10 20:18:49 +0300

    adding location properties

--- lib/pressentation/location.dart
+++ lib/pressentation/location.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:nasa/pressentation/common/basicappbar.dart';
 import 'package:nasa/pressentation/common/buttomNavigation.dart';
