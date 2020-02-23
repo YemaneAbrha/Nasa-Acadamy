003cbdd5654403e5f410f02e15a0918caf71cf03
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-23 17:07:28 +0300

    Working on API function

---
+++ lib/controller/apiFunction.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/widgets.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future getMessage(BuildContext context) async {
+  try {
+    final url = 'http://192.168.1.102:3000/message';
+    http.Response response = await http.get(url, headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    });
+    final message = jsonDecode(response.body);
+    return message;
+  } catch (e) {
+    print(e);
+  }
+}
+
+Future sendMessage(BuildContext context, Map<String, dynamic> body) async {
+  try {
+    final url = "here";
+    http.Response response = await http.post(
+      url,
+      body: body,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+    );
+    if (response.statusCode == 200) {
+      print("Sent Success fully");
+    } else {
+      print("Message Not Sent");
+    }
+  } catch (e) {
+    print(e);
+  }
+}
+
+Future getEvent(BuildContext context) async {
+  try {
+    final url = 'http://192.168.1.102:3000/events';
+    http.Response response = await http.get(url, headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    });
+    final event = jsonDecode(response.body);
+    return event;
+  } catch (e) {
+    print(e);
+  }
+}
