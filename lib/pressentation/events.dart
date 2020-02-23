3136a22004154e5b7fc9430b83621b97c8dcc31e
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-23 17:32:31 +0300

    Working on event

---
+++ lib/pressentation/events.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'package:nasa/pressentation/common/basicappbar.dart';
+import 'package:nasa/pressentation/common/buttomNavigation.dart';
+import 'package:nasa/pressentation/common/sidebar.dart';
+import 'package:nasa/controller/apiFunction.dart';
+
+class EventsPage extends StatefulWidget {
+  @override
+  _EventsPageState createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(235, 237, 240, 1.0),
+      appBar: BasicAppBar.getAppBar(context),
+      body: Container(
+        child: FutureBuilder(
+            future: getEvent(context),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return Container(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              } else {
+                return Container(
+                    child: Center(
+                  child: Text("There is data"),
+                ));
+              }
+            }),
+      ),
+      drawer: SideBarDrawer(),
+      bottomNavigationBar:
+          ButtomNavigationBarContainer.getButtomNavigationBar(context),
+    );
+  }
+}
