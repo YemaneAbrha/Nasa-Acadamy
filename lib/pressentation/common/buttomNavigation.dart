08333c61ef741b8f507ec56c749e3f90d2966efd
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-24 16:57:06 +0300

    Removing Static Method

--- lib/pressentation/common/buttomNavigation.dart
+++ lib/pressentation/common/buttomNavigation.dart
@@ -2,8 +2,26 @@
 // contain the buttom navigation
 import 'package:flutter/material.dart';
 
-class ButtomNavigationBarContainer {
-  static getButtomNavigationBar(BuildContext context) {
+class ButtomNavigationBarContainer extends StatefulWidget {
+  _ButtomNavigationBarContainerState createState() =>
+      _ButtomNavigationBarContainerState();
+}
+
+class _ButtomNavigationBarContainerState
+    extends State<ButtomNavigationBarContainer> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return getButtomNavigationBar(context);
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  getButtomNavigationBar(BuildContext context) {
     return BottomNavigationBar(
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
@@ -19,6 +37,9 @@ class ButtomNavigationBarContainer {
           title: Text("Notification"),
         ),
       ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
     );
   }
 }
