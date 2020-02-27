aa984dd25016595afdf829be8aff14ce565fdef5
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-27 16:52:15 +0300

    Complet get Message Function

--- lib/controller/apiFunction.dart
+++ lib/controller/apiFunction.dart
@@ -7,47 +7,18 @@ import 'dart:convert';
 import 'package:nasa/model/json/data.dart';
 
 Future getMessage(BuildContext context) async {
-  // try {
-  //   final url = 'http://10.0.2.2:3000/message';
-  //   http.Response response = await http.get(url, headers: {
-  //     "Accept": "application/json",
-  //     "Content-Type": "application/json"
-  //   });
-  //   final message = jsonDecode(response.body);
-  //   return message;
-  // } catch (e) {
-  //   print(e);
-  // }
   try {
-    final url = 'http://10.0.2.2:3000/events';
+    final url = 'http://192.168.137.42:8000/messages';
     http.Response response = await http.get(
       url,
       headers: {
         "Accept": "application/json",
-        // "Content-Type": "application/json"
       },
     );
-    final event = jsonDecode(response.body);
-    return event;
-  } catch (e) {}
-}
 
-Future sendMessage(BuildContext context, Map<String, dynamic> body) async {
-  try {
-    final url = "here";
-    http.Response response = await http.post(
-      url,
-      body: body,
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-    );
-    if (response.statusCode == 200) {
-      print("Sent Success fully");
-    } else {
-      print("Message Not Sent");
-    }
+    final event = jsonDecode(response.body);
+    print(event);
+    return event;
   } catch (e) {
     print(e);
   }
@@ -65,10 +36,6 @@ Future<List<Event>> getEvent(BuildContext context) async {
     );
     final datas = jsonDecode(response.body);
     List<Event> events = [];
-    // print(datas);
-    // for (var data in datas) {
-    //   print(data);
-    // }
     for (var data in datas) {
       print(data);
       Event event = Event(
