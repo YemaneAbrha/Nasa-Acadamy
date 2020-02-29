fa319f11b491c3c68a6998cbcf1d7c4edfa3000c
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-29 13:30:57 +0300

    Preparing Fort Release

--- lib/pressentation/body/location.dart
+++ lib/pressentation/body/location.dart
@@ -2,10 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import 'package:nasa/pressentation/common/basicappbar.dart';
-import 'package:nasa/pressentation/common/buttomNavigation.dart';
-import 'package:nasa/pressentation/common/sidebar.dart';
-
 class LocationPage extends StatefulWidget {
   @override
   _LocationPageState createState() => _LocationPageState();
@@ -15,27 +11,16 @@ class _LocationPageState extends State<LocationPage> {
   Completer<GoogleMapController> _controller = Completer();
   static final CameraPosition _kGoogleplex = CameraPosition(
     target: LatLng(8.988518, 38.831057),
-    zoom: 14.4746,
+    zoom: 20.4746,
   );
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(8.988518, 38.831057),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(235, 237, 240, 1.0),
-      appBar: BasicAppBar.getAppBar(context),
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGoogleplex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      drawer: SideBarDrawer(),
-      bottomNavigationBar: ButtomNavigationBarContainer(),
+    return GoogleMap(
+      mapType: MapType.hybrid,
+      initialCameraPosition: _kGoogleplex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 }
