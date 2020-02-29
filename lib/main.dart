f11cd3b5bf5a13fb51f68817bc75c4e7bd27633f
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-29 19:33:41 +0300

    Removing Debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 // From nasa package
-import 'package:nasa/pressentation/body/about.dart';
-import 'package:nasa/pressentation/body/location.dart';
+
 import 'package:nasa/pressentation/common.dart';
 
 void main() => runApp(MyApp());
@@ -11,11 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: <String, WidgetBuilder>{
-        "/about": (BuildContext context) => AboutPage(),
-        "/location": (BuildContext context) => LocationPage(),
-        // "/events": (BuildContext context) => EventsPage(),
-      },
+      debugShowCheckedModeBanner: false,
       title: 'ናሳ ት/ቤት',
       theme: ThemeData(
         primaryColor: Color.fromRGBO(0, 141, 81, 1.0),
