450b62acc721e79e01c56479b8be492b3fdcb689
YemaneAbrha <yemaneabrha21@gmail.com>
2020-02-29 13:54:22 +0300

    Preparing Fort Release

--- lib/pressentation/body/about.dart
+++ lib/pressentation/body/about.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nasa/pressentation/common/basicappbar.dart';
-import 'package:nasa/pressentation/common/buttomNavigation.dart';
-import 'package:nasa/pressentation/common/sidebar.dart';
 
 class AboutPage extends StatefulWidget {
   @override
@@ -13,7 +10,32 @@ class _AboutPageState extends State<AboutPage> {
   Widget build(BuildContext context) {
     return Container(
       child: ListView(
-        children: <Widget>[],
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.school),
+              title: Text("Vision"),
+              subtitle: Text(
+                  "The school will be a diverse community engaged in the in the process of preparing our students to live exemplary lives, committed to sharing wisdom, showing courage and supporting in the country in 2025."),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.school),
+              title: Text("Mission"),
+              subtitle: Text(
+                  "Nasa academy is dedicated to satisfy customers, deliver quality educational service create good public image , create good market share and develop employees in the next three years."),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.school),
+              title: Text("Mission"),
+              subtitle: Text(
+                  "Nasa academy is a diverse community preparing students to live exemplary lives, committed to sharing wisdom, showing courage, and promote in orientation. "),
+            ),
+          ),
+        ],
       ),
     );
   }
